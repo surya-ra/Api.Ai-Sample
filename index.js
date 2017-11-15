ac6a14660dfaeebcf2f4bcce876864faa2e3b1a3
surya <ess.dee3107@gmail.com>
2017-11-15 17:44:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
+    return res.json(
        'I am busy'
-    });
+    );
 });
 
 restService.post('/slack-test', function(req, res) {
